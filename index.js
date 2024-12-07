e71de29abaf58e589017132c2a92b9385b8ed6d6
emeritolopes <101825132+emeritolopes@users.noreply.github.com>
2024-12-07 22:34:09 +0000

    cards rendered out

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
+console.log("sumEl")
 function startGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = firstCard + " " + secondCard
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
